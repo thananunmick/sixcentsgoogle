1b501d1f96b97e080aad1cfac6d607a3774e9b70
Krirk Nirunwiroj <krirk_nirunwiroj@hotmail.com>
2022-03-21 17:07:56 -0500

    more flexibility

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:vibration/vibration.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return MaterialApp(
+        home: BootupPage());
+  }
+}
+
+
+class BootupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return Scaffold(
         backgroundColor: Colors.white,
         body: Center(
           child:
             Image.asset(
-                'images/SixCents_logo.png',
-              scale: 1.5
-            ),
-        )
+           'images/SixCents_logo.png',
+           scale: 1.5
+           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              ctxt,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+          backgroundColor: Colors.green,
+          child: const Icon(Icons.navigation),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home"),
+      ),
+      body: new Text("testtesttest"),
+    );
+  }
 }
